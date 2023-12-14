5e8b24134763ff2323efe280d2e096ad8e2bd897
masaru-gif <k22089kk@aitech.ac.jp>
2023-12-14 16:57:35 +0900

    diariesの中身追加
    main.pyも少しいじりました

---
+++ diaries/k22089Diary.py
@@ -0,0 +1,8 @@
+from diaries.AbstractDiary import AbstractDiary
+class k22089Diary(AbstractDiary):
+    def get_date(self):
+        return "2023-12-14"
+def get_summary(self):
+    return """今日は特に何もない一日だった"""
+def get_author(self):
+        return "k22089"
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from diaries.DiarySample import DiarySample
 from diaries.k22089Diary import k22089Diary
-# ↓のリストには、メンバーの各日記が格納されます。 
+from diaries.OdagiriDiary import OdagiriDiary
+
+# ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
     DiarySample(),
     k22089Diary(),
-]
+    OdagiriDiary(),
+    ]
+
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
