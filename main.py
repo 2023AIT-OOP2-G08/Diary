c37f65c9479364c7f37e69cce80262334205d717
masaru-gif <k22089kk@aitech.ac.jp>
2023-12-14 16:09:33 +0900

    first-pull

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from diaries.DiarySample import DiarySample
-
-# ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]#変更箇所
-
+from diaries.k22089Diary import k22089Diary
+# ↓のリストには、メンバーの各日記が格納されます。 
+diaries = [
+    DiarySample(),
+    k22089Diary(),
+]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
