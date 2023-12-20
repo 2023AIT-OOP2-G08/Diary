79e300c53d0f477bd8689a0d9f68f1d836ac93c9
shinno22085 <shin-nosuke0919@au.com>
2023-12-20 09:55:12 +0900

    Update TatsunoDiary.py

--- diaries/TatsunoDiary.py
+++ diaries/TatsunoDiary.py
@@ -2,7 +2,7 @@ from diaries.AbstractDiary import AbstractDiary
 
 class TatsunoDiary(AbstractDiary):
     def get_date(self):
-        return "2021-12-14"
+        return "2021-12-19"
     def get_summary(self):
         return """朝からずっとアニメを観ていた。時間を忘れてしまうほど、面白かった。"""
     def get_author(self):
